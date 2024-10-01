176d823c6250e333cc1637270200f25903e7146a
Talat <ta.rasouli60@yahoo.com>
2024-10-01 18:45:20 +0330

    feat:abstract programming

---
+++ asynchoronous-programming.py
@@ -0,0 +1,15 @@
+# import requests
+
+# response=requests.get('https://github.com/dgango')
+
+# header_items=response.headers.items()
+
+# res_headers=[f'{key}:{value}' for key,value in header_items]
+
+# formatted_headers='\n'.join(res_headers)
+
+# with open('headers.txt','w') as file:
+#     file.write(formatted_headers)
+
+# ----------------
+
