e1cd0e88c2346159dd096d9bb3c460bd6c034d9e
Talat <ta.rasouli60@yahoo.com>
2024-10-02 09:34:05 +0330

    feat:input output examples

---
+++ warlus.opretor.py
@@ -0,0 +1,26 @@
+# num=-1
+
+# num=input('Enter the number:')
+# while num<0:
+#   num=input('Enter the number:')
+
+# ------------------------------
+
+# while True:
+#     number=int(input('Enter a possitive number:'))
+#     if number>0:
+#         break
+# ------------------------------------
+# num=2
+# if num>0:
+#     print('possitive')
+# --------------------------------------
+# num=-1
+# num=int(input('enter a number:'))
+# if num<0:
+#     print(num)
+
+    
+
+
+  
